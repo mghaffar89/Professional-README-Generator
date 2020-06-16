c02409b740f78d7a3fb8e2d2c9f426170ff873b9
Mina G <mghaffar89@gmail.com>
2020-06-16 00:09:19 -0500

    npm install files

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
   {
