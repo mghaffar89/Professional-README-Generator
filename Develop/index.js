9fd932012b61414fe698372eef5afb84dd81a0ae
Mina G <mghaffar89@gmail.com>
2020-06-15 23:31:45 -0500

    added array of questions for the user to answer

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,62 @@
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "ProjectTitle",
+    message: "What would you like your Project Title to be named?",
+  },
+  {
+    type: "input",
+    name: "ProjectDescription",
+    message: "Please write a short description of your project.",
+  },
+  {
+    type: "input",
+    name: "Install",
+    message: "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Please provide examples of how your project could be used.",
+  },
+  {
+    type: "list",
+    name: "License",
+    message: "Choose what license you would like to use for your project.",
+    choices: [
+      {
+        name: "GNU General Public License v3.0",
+      },
+      {
+        name: "MIT License",
+      },
+      {
+        name: "The Unlicense",
+      },
+      {
+        name: "Apache License 2.0",
+      },
+    ],
+  },
+  {
+    type: "input",
+    name: "Contributors",
+    message: "How many contributors will there be on your project?",
+    validate: validateContributors,
+  },
+  {
+    type: "input",
+    name: "FAQ1",
+    message: "How do you initialize the project?",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
